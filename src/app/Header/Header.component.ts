875715603026cb2f78c659243dfff9f36235203e
Omar Hosny <honsyomar022@gmail.com>
2022-10-15 21:57:24 +0200

    FIREBASE ADDED

--- src/app/Header/Header.component.ts
+++ src/app/Header/Header.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { RecipeService } from '../recipes/recipe.service';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
 })
 export class HeaderComponent {
+  constructor(
+    private dataStorageSr: DataStorageService,
+    private recipeSr: RecipeService
+  ) {}
+  onSaveData() {
+    this.dataStorageSr.storeRecipes();
+  }
+  onFetchData() {
+    this.dataStorageSr.fetchRecipes().subscribe();
+  }
 }
